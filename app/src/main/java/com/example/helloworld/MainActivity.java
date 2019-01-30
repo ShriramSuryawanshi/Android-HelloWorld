67d102cb3cc92aab70d1204439a8cee6335af331
ShriramSuryawanshi <shriram.a.suryawanshi@gmail.com>
2019-01-29 23:49:57 -0500

    random Hi5 game

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,16 +1,18 @@
 package com.example.helloworld;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.support.v7.widget.Toolbar;
 import android.support.design.widget.Snackbar;
 import android.widget.EditText;
-
+import android.widget.TextView;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    boolean first = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,18 +26,43 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                String name = textbox.getText().toString();
-                name = "Hello "+ name +"!";
+                if (first) {
 
-                textbox.setText("");
+                    String name = textbox.getText().toString();
+                    textbox.setText("");
 
-                if (name.length() == 7)
-                    Snackbar.make(view, "Ohh, that's not a valid name!", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-                else
-                    Snackbar.make(view, (String)name, Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                    if (name.length() == 0)
+                        Snackbar.make(view, "Ohh, that can't be your name!", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                    else {
+                        name = "Hello " + name + "! Let's Hi5!!";
+                        Snackbar.make(view, (String) name, Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                        first = false;
+
+                        TextView label1 = findViewById(R.id.text_hello);
+                        TextView label2 = findViewById(R.id.text_name);
+
+                        textbox.setVisibility(View.GONE);
+                        label1.setVisibility(View.GONE);
+                        label2.setVisibility(View.GONE);
+                    }
+                }
+
+                if(!first) {
+
+                    Button button = findViewById(R.id.button);
+                    ConstraintLayout.LayoutParams params = (ConstraintLayout.LayoutParams) button.getLayoutParams();
+
+                    Random rand = new Random();
+                    float x = rand.nextFloat();
+                    float y = rand.nextFloat();
+
+                    params.horizontalBias = x;
+                    params.verticalBias = y;
+                    button.setLayoutParams(params);
+                }
             }
         });
-
-
     }
 }
+
+
